9c7b7d278b6458a48c6bc8a27e5b73174ed3d491
matthewcosgrove <mcosgrove@pivotal.io>
2017-05-29 12:09:22 +0100

    Conditional logic added based on env var

--- fixtures/script.sh
+++ fixtures/script.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-set -ex
+set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Version will be $PRODUCT_VERSION_ERT"
+echo "Script run will be $SCRIPT_NAME"
+
+source $SCRIPT_DIR/$SCRIPT_NAME
