7ed1a4e9e27c4b55f90561c03df5924ee232425c
matthewcosgrove <mcosgrove@pivotal.io>
2017-05-29 14:08:21 +0100

    Use release

--- tasks/task.sh
+++ tasks/task.sh
@@ -2,8 +2,7 @@
 set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-YML2ENV_TOOL="${SCRIPT_DIR}/yml2env-linux"
-wget -O $YML2ENV_TOOL -nc https://github.com/EngineerBetter/yml2env/releases/download/v1.1.0/yml2env_linux_x86-64
+YML2ENV_TOOL="${SCRIPT_DIR}/../../yml2env/yml2env_linux_x86-64"
 chmod +x $YML2ENV_TOOL
-cd poc-yml2env/fixtures
+cd ${SCRIPT_DIR}/../fixtures
 exec $YML2ENV_TOOL vars.yml ./script.sh
